443f2f4b762668ec5951d976936aecb4ca5795ee
PhoQ <btlp282@gmail.com>
2026-03-25 12:29:52 -0400

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+author 'PhoQ'
+description 'Forced First Person with Multiple Options.'
+version '1.0'
+
+client_script 'main.lua'
+server_script 'config.lua'
+shared_script 'config.lua'
